b1ec4c159b5cb9cf06c2d7ba031b2fc13136188f
Drew Ross <mail.drew.ross@gmail.com>
2020-04-15 12:44:06 -0700

    Add credit score.

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ let rate = 0.05;
 let years = 30;
 let monthlyInterestRate = rate / 12;
 let periods = years * 12;
+let creditScore = 700;
 const name = "Drew";
 
 
 
-const mortgageCalculator = (p, i, n) => {
+const mortgageCalculator = (p, i, n, credit) => {
+
+    if (credit > 740) {
+        i = (rate - 0.005) / 12;
+    } else if (credit < 660) {
+        i = (rate + 0.005) / 12;
+    }
+
     let numerator = ( i * Math.pow((1 + i), n) );
     let denominator = ( Math.pow((1 + i), n) - 1);
     let monthlyRate = ( p * (numerator / denominator) );
     return `${name}, your monthly rate is ${monthlyRate}`;
 }
 
-console.log(mortgageCalculator(200000, monthlyInterestRate, periods));
+console.log(mortgageCalculator(principal, monthlyInterestRate, periods, creditScore));
