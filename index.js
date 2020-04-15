c1c14752b611334d90f9234ca6b3f7c39368dc83
Drew Ross <mail.drew.ross@gmail.com>
2020-04-15 11:00:42 -0700

    Add JS variables.

---
+++ index.js
@@ -0,0 +1,13 @@
+//Variables
+let principal = 200000;
+let interestRate = 0.05;
+let monthlyInterestRate = interestRate / 12;
+let years = 30;
+let periods = years * 12;
+
+const name = "Drew";
+
+//Functions
+const mortgageCalculator = () => {
+
+}
