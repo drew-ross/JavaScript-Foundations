0154e8aa1251e622822596290f787e6257a7f6cf
Drew Ross <mail.drew.ross@gmail.com>
2020-04-15 12:31:26 -0700

    Add working calculator function.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-//Variables
 let principal = 200000;
-let interestRate = 0.05;
-let monthlyInterestRate = interestRate / 12;
+let rate = 0.05;
 let years = 30;
+let monthlyInterestRate = rate / 12;
 let periods = years * 12;
-
 const name = "Drew";
 
-//Functions
-const mortgageCalculator = () => {
 
+
+const mortgageCalculator = (p, i, n) => {
+    let numerator = ( i * Math.pow((1 + i), n) );
+    let denominator = ( Math.pow((1 + i), n) - 1);
+    let monthlyRate = ( p * (numerator / denominator) );
+    return `${name}, your monthly rate is ${monthlyRate}`;
 }
+
+console.log(mortgageCalculator(200000, monthlyInterestRate, periods));
