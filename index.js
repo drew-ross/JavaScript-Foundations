9cbfcd28648e71bde57d4d245a32f02ba6ba0940
Drew Ross <mail.drew.ross@gmail.com>
2020-04-15 13:14:07 -0700

    MVP

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 let principal = 200000;
 let rate = 0.05;
 let years = 30;
-let monthlyInterestRate = rate / 12;
 let periods = years * 12;
-let creditScore = 700;
+let creditScore = 600;
 const name = "Drew";
 
 
 
-const mortgageCalculator = (p, i, n, credit) => {
+const mortgageCalculator = (principal, rate, periods, credit) => {
 
     if (credit > 740) {
-        i = (rate - 0.005) / 12;
+        rate -= .005;
     } else if (credit < 660) {
-        i = (rate + 0.005) / 12;
+        rate += .005;
     }
+    let monthlyInterestRate = rate / 12;
 
-    let numerator = ( i * Math.pow((1 + i), n) );
-    let denominator = ( Math.pow((1 + i), n) - 1);
-    let monthlyRate = ( p * (numerator / denominator) );
-    return `${name}, your monthly rate is ${monthlyRate}`;
+    let numerator = ( monthlyInterestRate * Math.pow((1 + monthlyInterestRate), periods) );
+    let denominator = ( Math.pow((1 + monthlyInterestRate), periods) - 1);
+    let monthlyRate = ( principal * (numerator / denominator) );
+    return `${name}, your monthly rate is ${monthlyRate} with a credit score of ${credit}`;
 }
 
-console.log(mortgageCalculator(principal, monthlyInterestRate, periods, creditScore));
+const variableInterestRate = (principal, rate, periods) => {
+
+    console.log("Your input rate is " + rate);
+    rate -= .02;
+    let monthlyInterestRate = rate / 12;
+
+    for (let i = 0; i < 10; i++) {
+        let numerator = ( monthlyInterestRate * Math.pow((1 + monthlyInterestRate), periods) );
+        let denominator = ( Math.pow((1 + monthlyInterestRate), periods) - 1);
+        let monthlyRate = ( principal * (numerator / denominator) );
+        console.log(`${name}, at ${rate} interest rate, your monthly rate is ${monthlyRate}`);
+        
+        rate += .005;
+        monthlyInterestRate = rate / 12;
+    }
+
+    
+}
+
+console.log(mortgageCalculator(principal, rate, periods, creditScore));
+variableInterestRate(principal, .04, periods);
